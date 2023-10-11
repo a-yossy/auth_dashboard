4aa9e837081f4f15a56cb6752e5cb43e4d504883
a-yossy <atsuya0930@yahoo.co.jp>
2023-10-12 04:29:21 +0900

    404

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import { CenterTitle } from 'src/components/elements';
+
+const NotFoundErrorPage: NextPage = () => (
+  <CenterTitle>ページが見つかりません</CenterTitle>
+);
+
+export default NotFoundErrorPage;
