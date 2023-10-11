7e8b5d4b137bf96dc4c8b70674f27b39ad66c354
a-yossy <atsuya0930@yahoo.co.jp>
2023-10-12 03:27:52 +0900

    fix

--- jest.config.mjs
+++ jest.config.mjs
@@ -13,7 +13,6 @@ const config = {
 
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
